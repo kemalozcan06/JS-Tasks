9215f4c32355b2be8e55eee920c5812da46cf0ea
Kemal <103134273+kemalozcan06@users.noreply.github.com>
2023-04-12 19:16:54 +0300

    Delete JSLesson8 directory

--- JSLesson8/scripts.js
+++
@@ -1,9 +0,0 @@
-// let div = document.createElement('div')
-// document.body.appendChild(div)
-// document.querySelector('ul').appendChild(div)
-//  div.innerText = 'Hey'
-
- 
-//  let span = document.createElement('span');
-//  document.body.appendChild(span)
-//  span.innerText = 'Kemal'
